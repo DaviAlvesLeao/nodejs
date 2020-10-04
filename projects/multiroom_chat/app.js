215a68df2cc3ff614e3830acb513611c32c4e24c
davi <davileao20214@hotmail.com>
2020-10-04 17:12:31 -0300

    master

---
+++ projects/multiroom_chat/app.js
@@ -0,0 +1,8 @@
+/** Importar as configurações do servidor */
+var app = require('./config/server');
+app.listen(3000, function(){
+    console.log('Servidor ON')
+})
+
+
+
---
+++ projects/multiroom_chat/config/server.js
@@ -0,0 +1,36 @@
+/** Importando os modulos para o servidor */
+
+var express = require('express');
+var expressValidator = require('express-validator');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+
+
+/** Iniciando o app */
+
+var app = express();
+
+/**Configurações do app */
+app.set('view engine', ejs);
+app.set('views', './app/views');
+
+/** Configurar middleware express.static */
+app.use(express.static('./app/public'));
+
+/** Configurar middleware body-parser */
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*configurar o middleware express-validator */
+app.use(expressValidator());
+
+/** Efetua o autoload das rotas, models, controllers para o objeto app */
+consign()
+    .include('app/routes')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
+
+
+/** */
+module.exports = app;
